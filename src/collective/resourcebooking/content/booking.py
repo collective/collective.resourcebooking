ba7ca5977c4d0e425279922d70af8b6008da8334
MrTango <md@derico.de>
2023-01-26 14:12:43 +0200

    add start/end datetimefields to Booking

--- src/collective/resourcebooking/content/booking.py
+++ src/collective/resourcebooking/content/booking.py
@@ -1,62 +1,34 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
-from plone.dexterity.content import Item
-
-# from plone.namedfile import field as namedfile
-from plone.supermodel import model
-
 # from plone.supermodel.directives import fieldset
+# from plone.namedfile import field as namedfile
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from collective.resourcebooking import _
+from plone.dexterity.content import Item
+from plone.supermodel import model
+from zope import schema
 from zope.interface import implementer
-
-
-# from collective.resourcebooking import _
 
 
 class IBooking(model.Schema):
     """Marker interface and Dexterity Python Schema for Booking"""
 
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    start = schema.Datetime(
+        title=_(
+            u'Start',
+        ),
+        # defaultFactory=get_default_start,
+        required=True,
+    )
 
-    # model.load('booking.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    end = schema.Datetime(
+        title=_(
+            u'End',
+        ),
+        # defaultFactory=get_default_start,
+        required=True,
+    )
 
 
 @implementer(IBooking)

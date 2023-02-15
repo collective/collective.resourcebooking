0a25bded12b3b71f5b379a193e360aea71fea949
MrTango <md@derico.de>
2023-02-15 15:05:02 +0200

    finish addform with only minimal fields

--- src/collective/resourcebooking/forms/booking_default_add_form.py
+++ src/collective/resourcebooking/forms/booking_default_add_form.py
@@ -4,40 +4,38 @@ from plone.autoform.form import AutoExtensibleForm
 from z3c.form import button
 from z3c.form import form
 from zope.interface import Interface
+from plone.dexterity.browser import add
 
 
-class IBookingDefaultAddForm(Interface):
-    """Schema Interface for IBookingDefaultAddForm
-    Define your form fields here.
-    """
-
-    name = schema.TextLine(
-        title="Your name",
-    )
+ADDFORM_FIELDS = [
+    "title",
+    "ressource",
+    ]
 
 
-class BookingDefaultAddForm(AutoExtensibleForm, form.EditForm):
-    schema = IBookingDefaultAddForm
-    ignoreContext = True
+class BookingDefaultAddForm(add.DefaultAddForm):
+    autoGroups = False
+    portal_type = "Booking"
 
-    label = "What's your name?"
-    description = "Simple, sample form"
+    addform_fields = ADDFORM_FIELDS
 
-    @button.buttonAndHandler("Ok")
-    def handleApply(self, action):
-        data, errors = self.extractData()
-        if errors:
-            self.status = self.formErrorsMessage
-            return
+    def updateFieldsFromSchemata(self):
+        super().updateFieldsFromSchemata()
 
-        # Do something with valid data here
+        # we do not want the extra groups...
+        self.groups = ()
 
-        changes = self.applyChanges(data)
-        # Set status on this form page
-        # (this status message is not bind to the session and does not go thru redirects)
-        if changes:
-            self.status = "Settings saved"
+        # filter out fields which are not in addform_fields
+        for field_name in self.fields:
+            if field_name not in self.addform_fields:
+                self.fields = self.fields.omit(field_name)
 
-    @button.buttonAndHandler("Cancel")
-    def handleCancel(self, action):
-        """User canceled. Redirect back to the front page."""
+    def nextURL(self):
+        if self.immediate_view is not None:
+            return "{:s}/@@edit".format("/".join(self.immediate_view.split("/")[:-1]))
+        else:
+            return self.context.absolute_url()
+
+
+class BookingDefaultAddView(add.DefaultAddView):
+    form = BookingDefaultAddForm

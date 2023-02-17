c030dd9e1f36faf3daf36bd8e30f16ffd4fb6a43
MrTango <md@derico.de>
2023-02-17 13:39:20 +0200

    implement bookings_view

--- src/collective/resourcebooking/views/bookings_view.py
+++ src/collective/resourcebooking/views/bookings_view.py
@@ -3,9 +3,9 @@
 # from collective.resourcebooking import _
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
-
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+from datetime import date
+from dateutil.relativedelta import relativedelta, MO, SU
 
 
 class IBookingsView(Interface):
@@ -13,10 +13,45 @@ class IBookingsView(Interface):
 
 
 class BookingsView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('bookings_view.pt')
 
     def __call__(self):
-        # Implement your own actions:
+        week_dates = self.get_week_dates()
+        print(week_dates)
+        self.current_week_bookings = self.find_bookings(
+            week_dates["current_week"][0],
+            week_dates["current_week"][1],
+        )
+        self.next_week_bookings = self.find_bookings(
+            week_dates["next_week"][0],
+            week_dates["next_week"][1],
+        )
         return self.index()
+
+    def find_bookings(self, week_start, week_end):
+        bookings = api.content.find(
+            container=self.context,
+            portal_type="Booking",
+            start={
+                "query": week_end,
+                "range": "max",
+            },
+            end={
+                "query": week_start,
+                "range": "min",
+            }
+        )
+        return bookings
+
+    def get_week_dates(self):
+        today = date.today()
+        current_week_start = today+relativedelta(weekday=MO(-1))
+        current_week_end = today+relativedelta(weekday=SU)
+        next_week_start = today+relativedelta(weekday=MO(+1))
+        next_week_end = today+relativedelta(weekday=SU(+2))
+        nextnext_week_start = today+relativedelta(weekday=MO(+2))
+        nextnext_week_end = today+relativedelta(weekday=SU(+3))
+        return {
+            "current_week": (current_week_start, current_week_end),
+            "next_week": (next_week_start, next_week_end),
+            "nextnext_week": (nextnext_week_start, nextnext_week_end),
+        }

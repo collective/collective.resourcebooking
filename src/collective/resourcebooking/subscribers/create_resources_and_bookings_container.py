9bc4b3af6c4a0b853b98eb8830dde871e6f8d8b7
MrTango <md@derico.de>
2023-03-06 17:01:46 +0100

    disallow adding Resources/Bookings in ResourceBooking, create them in subscrib

--- src/collective/resourcebooking/subscribers/create_resources_and_bookings_container.py
+++ src/collective/resourcebooking/subscribers/create_resources_and_bookings_container.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_inner
 from collective.resourcebooking import logger
+from zope.component import createObject
+from zope.component.hooks import getSite
 from plone import api
 
 
 def handler(obj, event):
     """Event handler"""
     container = aq_inner(event.object)
-    api.content.create(
-        container=container, type="Bookings", id="bookings", title="Bookings"
+    portal = getSite()
+    portal_types = portal.portal_types
+    resources_id = portal_types.constructContent(
+        "Resources",
+        container,
+        "resources",
+        title="Resources",
     )
-    api.content.create(
-        container=container, type="Resources", id="resources", title="Resources"
+    bookings_id = portal_types.constructContent(
+        "Bookings",
+        container,
+        "bookings",
+        title="Bookings",
     )
     logger.info(
         f"Subscriber created bookings and resources containers in : {obj.absolute_url()}"
